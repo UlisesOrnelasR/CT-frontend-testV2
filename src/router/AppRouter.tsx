949a463e2d062f20c03bf2818aa5c4d774f7356f
UlisesOrnelasR <ornelas.ulises19@gmail.com>
2024-07-11 19:06:37 -0700

    chore: base navbar

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,11 +1,15 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import { DashboardPage } from "../pages/DashboardPage";
+import { Layout } from "../components";
 
 export const AppRouter = () => {
   return (
     <Routes>
       <Route path="/*" element={<Navigate to="/" replace />} />
-      <Route path="/" element={<DashboardPage />} />
+      <Route path="/" element={<Layout />}>
+        <Route path="/" element={<DashboardPage />} />
+        <Route path="/*" element={<Navigate to="/" />} />
+      </Route>
     </Routes>
   );
 };
